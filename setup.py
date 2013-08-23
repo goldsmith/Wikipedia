0fb612c08d61da78bb0e07a3be40181a91adc738
Jonathan Goldsmith <jhghank@gmail.com>
2013-08-22 19:02:06 -0700

    set up shop in the cheese shop

---
+++ setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+import os 
+
+from setuptools import setup
+from io import open
+
+
+long_description = open(os.path.join(os.path.dirname(__file__), "README.rst"), encoding='utf-8').read().encode('ascii', 'ignore')
+
+dependencies = open(os.path.join(os.path.dirname(__file__), "requirements.txt")).readlines()
+
+setup(
+    name = "wikipedia",
+    version = "0.9.3",
+    author = "Jonathan Goldsmith",
+    author_email = "jhghank@gmail.com",
+    description = "Wikipedia API for Python",
+    license = "MIT",
+    keywords = "python wikipedia API",
+    url = "https://github.com/goldsmith/Wikipedia",
+    install_requires = dependencies,
+    packages = ['wikipedia', 'tests'],
+    long_description = long_description,
+    classifiers = (
+        'Development Status :: 3 - Alpha',
+        'Topic :: Software Development :: Libraries',
+        'License :: OSI Approved :: MIT License',
+    ),
+)
