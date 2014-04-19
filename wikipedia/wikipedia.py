eeb33f812e09ff8f56c52c599e98b3f63add1763
Kazuar <tzahi.vidas@gmail.com>
2014-04-19 23:35:13 +0300

    Added link to mediawiki geo data in search_loc method.

--- wikipedia/wikipedia.py
+++ wikipedia/wikipedia.py
@@ -64,7 +64,8 @@ def set_rate_limiting(rate_limit, min_wait=timedelta(milliseconds=50)):
 @cache
 def search_loc(latitude, longitude, article_name = None, results=10, radius=1000):
   '''
-  Do a wikipedia geo search for latitude and longitude
+  Do a wikipedia geo search for latitude and longitude 
+  using HTTP API described in http://www.mediawiki.org/wiki/Extension:GeoData
 
   Keyword arguments:
 
