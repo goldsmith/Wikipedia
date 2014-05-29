9e5f1f7e75e9301d2492933bddbe8298db19a866
Jonathan Goldsmith <jhghank@gmail.com>
2014-05-29 11:59:12 -0700

    refactor setup.py and add __version__ (v1.3.0!)

--- setup.py
+++ setup.py
@@ -1,20 +1,27 @@
 # -*- coding: utf-8 -*-
 import os
 import codecs
+import setuptools
 
-from setuptools import setup
+import wikipedia
 
-long_description = codecs.open(os.path.join(os.path.dirname(__file__), 'README.rst'), 'r', 'utf-8').read()
+
+def local_file(file):
+  return codecs.open(
+    os.path.join(os.path.dirname(__file__), file), 'r', 'utf-8'
+  )
 
 install_reqs = [
   line.strip()
-  for line in open(os.path.join(os.path.dirname(__file__), 'requirements.txt')).readlines()
+  for line in local_file('requirements.txt').readlines()
   if line.strip() != ''
 ]
 
-setup(
+version = '.'.join(map(str, wikipedia.__version__))
+
+setuptools.setup(
   name = "wikipedia",
-  version = "1.3dev",
+  version = version,
   author = "Jonathan Goldsmith",
   author_email = "jhghank@gmail.com",
   description = "Wikipedia API for Python",
@@ -23,12 +30,12 @@ setup(
   url = "https://github.com/goldsmith/Wikipedia",
   install_requires = install_reqs,
   packages = ['wikipedia'],
-  long_description = long_description,
+  long_description = local_file('README.rst').read(),
   classifiers = [
     'Development Status :: 4 - Beta',
     'Topic :: Software Development :: Libraries',
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python',
     'Programming Language :: Python :: 3'
-  ],
+  ]
 )
--- wikipedia/__init__.py
+++ wikipedia/__init__.py
@@ -1,2 +1,4 @@
 from .wikipedia import *
 from .exceptions import *
+
+__version__ = (1, 3, 0)
