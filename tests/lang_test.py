d12388d60f3a25a52b87386b3e72fd6144e68d46
camgza <camgza@gmail.com>
2014-08-29 16:05:51 +1200

    ran project through autopep8 to fix the indentation

--- tests/lang_test.py
+++ tests/lang_test.py
@@ -5,8 +5,10 @@ from wikipedia import wikipedia
 
 
 class TestLang(unittest.TestCase):
-  """Test the ability for wikipedia to change the language of the API being accessed."""
 
-  def test_lang(self):
-    wikipedia.set_lang("fr")
-    self.assertEqual(wikipedia.API_URL, 'http://fr.wikipedia.org/w/api.php')
+    """Test the ability for wikipedia to change the language of the API being accessed."""
+
+    def test_lang(self):
+        wikipedia.set_lang("fr")
+        self.assertEqual(
+            wikipedia.API_URL, 'http://fr.wikipedia.org/w/api.php')
