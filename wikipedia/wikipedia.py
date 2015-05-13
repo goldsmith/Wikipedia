cee4165e9c074680d86b0e4c48d41740cf2d6aeb
Caoyawen <bobcyw@163.com>
2015-05-14 06:25:57 +0800

    Update wikipedia.py
    make it pass in python 2

--- wikipedia/wikipedia.py
+++ wikipedia/wikipedia.py
@@ -11,7 +11,7 @@ from .exceptions import (
     WikipediaException, ODD_ERROR_MESSAGE)
 from .util import cache, stdout_encode, debug
 import re
-from urllib.parse import urljoin
+from urlparse import urljoin
 
 API_URL = 'http://en.wikipedia.org/w/api.php'
 RATE_LIMIT = False
