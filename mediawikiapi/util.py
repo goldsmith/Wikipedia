ec3ba3b75be3696f695f5218fcfb52b31e70789f
Taras Lehinevych <tleginevych@gmail.com>
2017-02-26 02:48:31 +0200

    rename cache to memorize and add support for instance methods

--- mediawikiapi/util.py
+++ mediawikiapi/util.py
@@ -1,7 +1,9 @@
 from __future__ import print_function, unicode_literals
 
 import sys
+import collections
 import functools
+
 
 def debug(fn):
   def wrapper(*args, **kwargs):
@@ -21,24 +23,35 @@ class Singleton(type):
     return cls._instances[cls]
 
 
-class cache(object):
-
-  def __init__(self, fn):
-    self.fn = fn
-    self._cache = {}
-    functools.update_wrapper(self, fn)
-
+class memorized(object):
+  '''Decorator. Caches a function's return value each time it is called.
+     If called later with the same arguments, the cached value is returned
+     (not reevaluated).
+  '''
+  def __init__(self, func):
+    self.func = func
+    self.cache = {}
+  
   def __call__(self, *args, **kwargs):
+    if not isinstance(args, collections.Hashable):
+      # uncacheable. a list, for instance.
+      # better to not cache than blow up.
+      return self.func(*args, **kwargs)
     key = str(args) + str(kwargs)
-    if key in self._cache:
-      ret = self._cache[key]
+    if key in self.cache:
+      return self.cache[key]
     else:
-      ret = self._cache[key] = self.fn(*args, **kwargs)
-
-    return ret
-
-  def clear_cache(self):
-    self._cache = {}
+      value = self.func(*args, **kwargs)
+      self.cache[key] = value
+      return value
+  
+  def __repr__(self):
+    '''Return the function's docstring.'''
+    return self.func.__doc__
+  
+  def __get__(self, obj, objtype):
+    '''Support instance methods.'''
+    return functools.partial(self.__call__, obj)
 
 
 # from http://stackoverflow.com/questions/3627793/best-output-type-and-encoding-practices-for-repr-functions
