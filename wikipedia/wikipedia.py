3e6ed1698d4ca1de4db7913481d70e846253343a
Anna Katrine Jørgensen <jorgensen@wcw-self-145-109-125-108.wireless.uva.nl>
2019-11-15 13:12:45 +0100

    syncing with head

--- wikipedia/wikipedia.py
+++ wikipedia/wikipedia.py
@@ -12,7 +12,6 @@ from .exceptions import (
 from .util import cache, stdout_encode, debug
 import re
 
-# anna
 API_URL = 'http://en.wikipedia.org/w/api.php'
 RATE_LIMIT = False
 RATE_LIMIT_MIN_WAIT = None
@@ -387,7 +386,7 @@ class WikipediaPage(object):
       request = _wiki_request(query_params)
       html = request['query']['pages'][pageid]['revisions'][0]['*']
 
-      lis = BeautifulSoup(html).find_all('li')
+      lis = BeautifulSoup(html, 'html.parser').find_all('li')
       filtered_lis = [li for li in lis if not 'tocsection' in ''.join(li.get('class', []))]
       may_refer_to = [li.a.get_text() for li in filtered_lis if li.a]
 
